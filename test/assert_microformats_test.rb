1ddd3c7e32e516f92a832f4baf50dd7331c4160a
George Brocklehurst <george.brocklehurst@gmail.com>
2009-05-30 16:12:34 +0100

    Added basic tests around distinguishing between different types of microformats.

--- test/assert_microformats_test.rb
+++ test/assert_microformats_test.rb
@@ -12,4 +12,16 @@ class AssertMicroformatsTest < Test::Unit::TestCase
     assert_microformat '<span class="vcard"><span class="fn">George Brocklehurst</span></span>', :hcard
   end
 
+  def test_should_not_find_hcalendar_when_only_hcard_is_present
+    assert_raise(Test::Unit::AssertionFailedError) do
+      assert_microformat '<span class="vcard"><span class="fn">George Brocklehurst</span></span>', :hcalendar
+    end
+  end
+
+  def test_should_raise_when_microformat_type_is_invalid
+    assert_raise(ArgumentError) do
+      assert_microformat '', :invalid_microformat_type
+    end
+  end
+
 end
