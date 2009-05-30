88f1f0dff9ec939d6bd3088858d7a7f2235ab3cb
George Brocklehurst <george.brocklehurst@gmail.com>
2009-05-30 15:46:13 +0100

    Added initialisation code for the plugin.
    Load the AssertMicroformats module and include it into the Test::Unit::TestCase class.

--- init.rb
+++ init.rb
@@ -1,1 +1,5 @@
-# Include hook code here
+# inject assert_microformats into test::unit for test environment
+if RAILS_ENV == 'test'
+  require 'assert_microformats'
+  Test::Unit::TestCase.send(:include, AssertMicroformats)
+end
