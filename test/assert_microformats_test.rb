701bdb643eaba845e92c0d344671f7238f5f5a10
George Brocklehurst <george.brocklehurst@gmail.com>
2009-05-30 15:31:21 +0100

    A few basic tests.

--- test/assert_microformats_test.rb
+++ test/assert_microformats_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
-class AssertMicroformatsTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class AssertMicroformatsTest < Test::Unit::TestCase
+
+  def test_should_fail_to_find_an_hcard_in_an_empty_string
+    assert_raise(Test::Unit::AssertionFailedError) do
+      assert_microformat '', :hcard
+    end
   end
+
+  def test_should_find_a_simple_hcard
+    assert_microformat '<span class="vcard"><span class="fn">George Brocklehurst</span></span>', :hcard
+  end
+
 end
