7c2b227da28e599e6fce85cf1051c63d7708dc9f
George Brocklehurst <george.brocklehurst@gmail.com>
2009-05-30 16:13:20 +0100

    Updated implementation to support different types of microformat.

--- lib/assert_microformats.rb
+++ lib/assert_microformats.rb
@@ -3,8 +3,20 @@ require 'mofo'
 module AssertMicroformats
 
   def assert_microformat(html, type, options = nil)
-    hcards = hCard.find :text => html
-    assert hcards.is_a?(hCard) || hcards.length > 0
+    mf = case type
+      when :hcard then hCard
+      when :hcalendar then hCalendar
+      when :hreview then hReview
+      when :hentry then hEntry
+      when :hresume then hResume
+      when :xoxo then XOXO
+      when :geo then Geo
+      when :xfn then XFN
+      else raise ArgumentError.new('Unsupported microformat type')
+    end
+
+    instances = mf.find :text => html
+    assert instances.is_a?(mf) || instances.length > 0
   end
 
 end
